14fbb9dc7c7ce5507ba3a2592cf509ba45a98673
Rahul Mishra <rahul.mishra@vectoscalar.com>
2020-04-30 09:43:43 +0530

    Mention changes and lint issues

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,28 +4,74 @@
  */
 
 // The editor creator to use.
-import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
-import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
-import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
-import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-import Indent from "@ckeditor/ckeditor5-indent/src/indent";
-import Link from "@ckeditor/ckeditor5-link/src/link";
-import List from "@ckeditor/ckeditor5-list/src/list";
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
-import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
-import Mention from "@ckeditor/ckeditor5-mention/src/mention";
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
 import GFMDataProcessor from 'ckeditormarkdownlinebreak/src/gfmdataprocessor';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 function Markdown( editor ) {
-	editor.d
-    editor.data.processor = new GFMDataProcessor( editor.editing.view.document );
+	editor.data.processor = new GFMDataProcessor( editor.editing.view.document );
+}
+
+function MentionCustomization( editor ) {
+	// The upcast converter will convert <a class='mention' href='' data-user-id=''>
+	// elements to the model 'mention' attribute.
+	editor.conversion.for( 'upcast' ).elementToAttribute( {
+		view: {
+			name: 'span',
+			key: 'data-mention',
+			classes: 'lh-user-label',
+			attributes: {
+				'data-value': true
+			}
+		},
+		model: {
+			key: 'mention',
+			value: viewItem => {
+				// The mention feature expects that the mention attribute value
+				// in the model is a plain object with a set of additional attributes.
+				// In order to create a proper object, use the toMentionAttribute helper method:
+				const mentionAttribute = editor.plugins.get( 'Mention' ).toMentionAttribute( viewItem, {
+					// Add any other properties that you need.
+					dataValue: viewItem.getAttribute( 'data-value' )
+				} );
+
+				return mentionAttribute;
+			}
+		},
+		converterPriority: 'high'
+	} );
+
+	// Downcast the model 'mention' text attribute to a view <a> element.
+	editor.conversion.for( 'downcast' ).attributeToElement( {
+		model: 'mention',
+		view: ( modelAttributeValue, viewWriter ) => {
+			// Do not convert empty attributes (lack of value means no mention).
+			if ( !modelAttributeValue ) {
+				return;
+			}
+
+			return viewWriter.createAttributeElement( 'span', {
+				class: 'lh-user-label',
+				'data-value': modelAttributeValue.userId
+			} );
+		},
+		converterPriority: 'high'
+	} );
 }
 
 // Plugins to include in the build.
@@ -43,29 +89,30 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	TextTransformation,
 	Mention,
-	Markdown
+	Markdown,
+	MentionCustomization
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			"heading",
-			"|",
-			"bold",
-			"italic",
-			"link",
-			"bulletedList",
-			"numberedList",
-			"|",
-			"indent",
-			"outdent",
-			"|",
-			"blockQuote",
-			"undo",
-			"redo",
-		],
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'indent',
+			'outdent',
+			'|',
+			'blockQuote',
+			'undo',
+			'redo'
+		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: "en",
+	language: 'en'
 };
